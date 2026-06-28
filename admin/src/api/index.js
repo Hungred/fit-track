c539d92f674bb49e7ed5cb691940f89ff59ca069
Anita <anita397892@gmail.com>
2026-06-28 22:58:55 +0800

    fix: 401 自動清除登入狀態並跳回登入頁

--- admin/src/api/index.js
+++ admin/src/api/index.js
@@ -4,6 +4,18 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 })
 
+api.interceptors.response.use(
+  res => res,
+  err => {
+    if (err.response?.status === 401) {
+      localStorage.removeItem('coach_uid')
+      localStorage.removeItem('coach_name')
+      window.location.href = '/login'
+    }
+    return Promise.reject(err)
+  }
+)
+
 export function setCoachHeader(lineUid) {
   api.defaults.headers.common['x-line-uid'] = lineUid
 }
